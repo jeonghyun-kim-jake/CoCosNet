c2af398a02c1c7baf27fef1c02f6ebd00b893239
Jeonghyun Kim <61818760+jeonghyun-kim-jake@users.noreply.github.com>
2020-11-12 23:08:41 +0900

    Update bf_ade20k_dataset.py

--- data/bf_ade20k_dataset.py
+++ data/bf_ade20k_dataset.py
@@ -1,6 +1,16 @@
 import os
+import random
 from data.pix2pix_dataset import Pix2pixDataset
 from data.image_folder import make_dataset
+
+from data.base_dataset import BaseDataset, get_params, get_transform
+
+import torch
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from os.path import isfile, join, abspath
 
 
 class BFADE20KDataset(Pix2pixDataset):
@@ -22,19 +32,26 @@ class BFADE20KDataset(Pix2pixDataset):
         return parser
 
     def get_paths(self, opt):
-        root = opt.dataroot
-        #subfolder = 'validation' if opt.phase == 'test' else 'training'
-        cache = False if opt.phase == 'test' else True
-        all_images = sorted(make_dataset(root , recursive=True, read_cache=cache, write_cache=False))
-        image_paths = []
-        label_paths = []
-        for p in all_images:
-            if p.endswith('.jpg'):
-                image_paths.append(p)
-            elif p.endswith('.png'):
-                label_paths.append(p)
+        instance_dir = opt.instance_dir
+        instance_paths = make_dataset(instance_dir, recursive=False, read_cache=True)
 
-        return label_paths, image_paths
+        image_dir = opt.image_dir
+        image_paths = make_dataset(image_dir, recursive=False, read_cache=True)
+
+        image_files = []
+        instance_files = []
+
+        for p in image_paths:            
+          if p.endswith('.jpg') or  p.endswith('.png') :
+            image_files.append(p)
+        
+        for p in instance_paths:             
+          if p.endswith('.png') and not  p.endswith('.png.png') :
+            instance_files.append(p)
+
+        assert len(instance_files) == len(image_files), "The #images in {} and {} do not match.".format(len(instance_files),len(image_files))
+
+        return instance_files, image_files
 
     def get_ref(self, opt):
         extra = '_test' if opt.phase == 'test' else ''
@@ -51,3 +68,60 @@ class BFADE20KDataset(Pix2pixDataset):
             ref_dict[key] = val
         train_test_folder = ('training', 'validation')
         return ref_dict, train_test_folder
+
+    def __getitem__(self, index):
+        # Label Image
+        label_path = self.label_paths[index]
+        label_tensor, params1 = self.get_label_tensor(label_path)
+        file_name = os.path.basename(label_path)
+
+        # input image (real images)
+        image_path = self.image_paths[index]
+        if not self.opt.no_pairing_check:
+            assert self.paths_match(label_path, image_path), \
+                "The label_path %s and image_path %s don't match." % \
+                (label_path, image_path)
+        image = Image.open(image_path)
+        image = image.convert('RGB')
+        
+        image.save("image.png")
+
+        transform_image = get_transform(self.opt, params1)
+        image_tensor = transform_image(image)
+
+        ref_tensor = 0
+        label_ref_tensor = 0
+
+        segment_path = join(self.opt.segment_dir, file_name)
+        path_ref = image_path
+
+        image_ref = Image.open(path_ref).convert('RGB')
+
+        path_ref_label = segment_path
+
+        label_ref_tensor, params = self.get_label_tensor(path_ref_label)
+        transform_image = get_transform(self.opt, params)
+
+        ref_tensor = transform_image(image_ref)
+
+        self_ref_flag = torch.zeros_like(ref_tensor)
+
+
+
+        input_dict = {'label': label_tensor,
+                      'image': image_tensor,
+                      'path': image_path,
+                      'self_ref': self_ref_flag,
+                      'ref': ref_tensor,
+                      'label_ref': label_ref_tensor
+                      }
+                      
+        print("\n\n====")
+        print("image_path", image_path)
+        print("label_path", label_path)
+        print("segment_path", segment_path)
+        print("====\n\n")
+        # Give subclasses a chance to modify the final output
+        self.postprocess(input_dict)
+
+        return input_dict
